f3c7c1711b1a7b1dc628daa1b2500549b98e54cd
ZeiZel <valera2003lvov@yandex.ru>
2023-08-14 14:10:57 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import './styles.css'
+import styled from 'styled-components/native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.textStyles}>Hello</Text>
+			<Text
+				style={{
+					fontSize: 16,
+				}}
+			>
+				world!
+			</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '100%',
+		height: '100%',
+	},
+	textStyles: {
+		color: 'purple',
+		fontSize: 24,
+	},
 });
